c3bcd19be06ae9a04d9c57741d18489e800b97e0
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-09-25 21:36:36 +0000

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+//import { set } from "cypress/types/lodash";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>To-Do List</h2>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+        {todos.map((todo, index) => (
+          <div key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </div>
+        ))}
     </div>
-  )
+  );
 }
 
 export default App
